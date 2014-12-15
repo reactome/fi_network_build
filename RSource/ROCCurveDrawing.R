bbc60ff40ff0b086a1511c5c72a516ebdbcbe911
gwu <>
2014-12-15 16:43:14 +0000

    updated script

--- RSource/ROCCurveDrawing.R
+++ RSource/ROCCurveDrawing.R
@@ -10,16 +10,18 @@ draw.thresholds <- function(points) {
 
 calculate.AUC <- function(rocData) {
 	# Need to use ROC package from bioconductor
+    require("ROC");
 	order <- order(rocData$False_Positive_Rate);
 	auc <- trapezint(rocData$False_Positive_Rate[order], rocData$True_Positive_Rate[order], 0, 1);
 	print(paste("AUC: ", auc, sep=""));
 }
 
 #fileName <- "/Users/wgm/v3/ROC_100_072809.txt";
-fileName <- "/Users/gwu/Documents/EclipseWorkspace/FINetworkBuild/results/2012/ROC_100_071012.txt";
+# fileName <- "/Users/gwu/Documents/EclipseWorkspace/FINetworkBuild/results/2012/ROC_100_071012.txt";
+fileName <- "/Users/gwu/Documents/EclipseWorkspace/FINetworkBuild/results/2013/ROC_100_120913.txt";
 rocData <- read.table(fileName, sep="\t", header=TRUE);
 ## Draw x-y plot
 #plot(rocData$False_Positive_Rate, rocData$True_Positive_Rate, type="l", main="NBC ROC Curve", xlab="False Positive Rate", ylab="True Positive Rate", xlim=c(0.00185, 0.05), ylim=c(0.035, 0.965));
-plot(rocData$False_Positive_Rate, rocData$True_Positive_Rate, type="l", main="NBC ROC Curve", xlab="False Positive Rate", ylab="True Positive Rate", xlim=c(0.0, 0.05), ylim=c(0.035, 0.5));
+plot(rocData$False_Positive_Rate, rocData$True_Positive_Rate, type="l", main="NBC ROC Curve", xlab="False Positive Rate", ylab="True Positive Rate", xlim=c(0.035, 1.0), ylim=c(0.035, 0.965));
 ## Draw three lines with cutoff values 0.25 (26th), 0.50 (51th), 0.75(76th)
 #draw.thresholds(c(26, 51, 76));
